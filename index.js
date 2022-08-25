6ebdd617ac922620cd6720134e5e2dc415fc66d4
priyalpatel2709 <piyu9552@gmail.com>
2022-08-25 14:03:48 +0530

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
+// const http = require("http");
+// const express = require("express");
+// const cros = require("cors");
+// const socketIO = require("socket.io");
+
+// const app = express();
+// const port =process.env.PORT;
+// const users = [{}];
+
+// app.use(cros());
+// app.get("/", (req, res) => {
+//   res.send("HELL ITS WORKING");
+// });
+
+// const server = http.createServer(app);
+
+// const io = socketIO(server);
+
+// io.on("connection", (socket) => {
+//   console.log("New Connection ");
+
+//   socket.on("joined", ({ user }) => {
+//     users[socket.id] = user;
+//     console.log(`${user} has joined`);
+//     socket.broadcast.emit("userJoined", {
+//       user: "Admin",
+//       message: `${users[socket.id]} has joined`
+//     });
+//     socket.emit("welcome", {
+//       user: "Admin",
+//       message: `welcome to the chat ${users[socket.id]}`
+//     });
+//     socket.emit("typing", {
+//       user: `Admin`,
+//       message: `${users[socket.id]} is typing`
+//     });
+//   });
+//   socket.on('message',({message,id})=>{
+//    io.emit('sentMessage',{user :users[id],message,id})
+//   })
+
+
+//   socket.on("disconnect", () => {
+//     socket.broadcast.emit("leave", {
+//       user: "Admin",
+//       message: ` ${users[socket.id]} has left `
+//     });
+//     console.log(`${users[socket.id]} has left`);
+//   });
+// });
+
+// server.listen(port, () => {
+//   console.log(`http://localhost:4000`);
+// });
 const http = require("http");
 const express = require("express");
 const cros = require("cors");
 const socketIO = require("socket.io");
+const { log } = require("console");
 
 const app = express();
 const port =process.env.PORT;
@@ -15,42 +70,46 @@ app.get("/", (req, res) => {
 const server = http.createServer(app);
 
 const io = socketIO(server);
-
+var clients = 0;
 io.on("connection", (socket) => {
   console.log("New Connection ");
-
+  clients++;
+  io.sockets.emit("broadcast", { clients });
+  socket.on("disconnect", function () {
+    clients--;
+    io.sockets.emit("broadcast", { clients });
+  });
   socket.on("joined", ({ user }) => {
     users[socket.id] = user;
     console.log(`${user} has joined`);
-    socket.broadcast.emit("userJoined", {
+
+    io.sockets.emit("joinandleft", {
       user: "Admin",
-      message: `${users[socket.id]} has joined`
+      message: `${user} has joined`,
+      m: user
+    });
+    socket.on("disconnect", function () {
+      io.sockets.emit("joinandleft", {
+        user: "Admin",
+        message: `${user} has left`,
+        m: user
+      });
     });
     socket.emit("welcome", {
       user: "Admin",
       message: `welcome to the chat ${users[socket.id]}`
     });
-    socket.emit("typing", {
-      user: `Admin`,
-      message: `${users[socket.id]} is typing`
-    });
   });
-  socket.on('message',({message,id})=>{
-   io.emit('sentMessage',{user :users[id],message,id})
-  })
-
-
+  socket.on("message", ({ message, id }) => {
+    io.emit("sentMessage", { user: users[id], message, id });
+  });
   socket.on("disconnect", () => {
-    socket.broadcast.emit("leave", {
-      user: "Admin",
-      message: ` ${users[socket.id]} has left `
-    });
     console.log(`${users[socket.id]} has left`);
   });
 });
 
 server.listen(port, () => {
-  console.log(`http://localhost:4000`);
+  console.log(`http://localhost:${port}`);
 });
 
 
